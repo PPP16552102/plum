a9b3f7fc7199c36e02c641997e2f45b8c4174a85
潘宇 <12275622+pan-yu-peter@user.noreply.gitee.com>
2024-12-20 01:12:38 +0800

    feat: init sketch

---
+++ scripts/gen-component-declaration.js
@@ -0,0 +1,6 @@
+import path from 'node:path';
+import process from 'node:process';
+import fs from 'fs-extra';
+import * as globalComponents from '../src/components';
+
+const TYPE_ROOT = process.cwd();
---
+++ vite.config.js
@@ -0,0 +1,5 @@
+const path = require('path');
+const { babel } = require('@rollup/plugin-babel');
+const dns = require('dns');
+
+dns.setDefaultResultOrder('verbatim');
